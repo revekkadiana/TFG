75a188d09bbcac93ee0aa56c98ddd0728cac8c3c
revekkadiana <uo289025@uniovi.es>
2025-02-16 11:15:06 +0100

    sitemaps

--- libreria_newspaper/procesar_url.py
+++ libreria_newspaper/procesar_url.py
@@ -1,42 +1,151 @@
-from newspaper import build
+# from newspaper import build
+# from datetime import datetime
+# from normalizar_urls import normalizar_url
+
+# def procesar_url(url, urls_pasadas, fecha_mas_reciente):
+#     print(f"\nProcesando sitio: {url}")
+#     # articulos = []
+#     articulos=set()
+
+#     try:
+#         config = Config()
+#         config.browser_user_agent = 'Mozilla/5.0 (Linux; Android 10; Pixel 3 XL Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36'
+#         config.request_timeout = 10
+#         config.language= 'es'
+#         config.request_timeout = 5
+#         config.thread_timeout_seconds = 5
+#         config.memoize_articles = False
+#         config.fetch_images = False
+#         config.follow_meta_refresh = True
+#         config.number_threads = 16
+#         config.headers = {
+#             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+#             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+#             "Accept-Language": "en-US,en;q=0.5",
+#             "Accept-Encoding": "gzip, deflate",
+#             "Connection": "keep-alive",
+#             "Upgrade-Insecure-Requests": "1",
+#             "Sec-Fetch-Dest": "document",
+#             "Sec-Fetch-Mode": "navigate",
+#             "Sec-Fetch-Site": "none",
+#             "Sec-Fetch-User": "?1",
+#             "Cache-Control": "max-age=0",
+#         }
+#         paper = build(url, language='es', config=config)
+#         for article in paper.articles:
+#             url_articulo = normalizar_url(article.url)
+
+#             if url_articulo in urls_pasadas:
+#                 print(f"Artículo ya procesado previamente, ignorado: {url_articulo}")
+#                 continue
+
+#             try:
+#                 article.download()
+#                 article.parse()
+
+#                 titulo = article.title
+#                 fecha_publicacion = article.publish_date
+
+#                 if not fecha_publicacion:
+#                     fecha_publicacion = 'Desconocido'
+#                 else:
+#                     fecha_publicacion = fecha_publicacion.strftime('%Y-%m-%d')
+
+#                     if fecha_mas_reciente and datetime.strptime(fecha_publicacion, '%Y-%m-%d') < fecha_mas_reciente:
+#                         print("Artículo ignorado porque es más antiguo que la fecha más reciente registrada.")
+#                         continue
+
+#                 articulos.add((titulo, fecha_publicacion, url_articulo))
+
+#             except Exception as e:
+#                 print(f"Error procesando el artículo: {e}")
+
+#     except Exception as e:
+#         print(f"Error procesando el sitio: {e}")
+
+#     return articulos
+
+
+from newspaper import build, Config, news_pool
 from datetime import datetime
+import requests
 from normalizar_urls import normalizar_url
+import time
 
 def procesar_url(url, urls_pasadas, fecha_mas_reciente):
     print(f"\nProcesando sitio: {url}")
-    articulos = []
+    articulos = set()
+    errores = []
 
     try:
-        paper = build(url, language='es', memoize_articles=False)
+        config = Config()
+        config.browser_user_agent = ('Mozilla/5.0 (Linux; Android 10; Pixel 3 XL Build/QP1A.190711.020) '
+                                     'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36')
+        config.request_timeout = 10
+        config.language = 'es'
+        config.memoize_articles = False
+        config.fetch_images = False
+        config.follow_meta_refresh = True
+        config.number_threads = 16
+        config.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Cache-Control": "max-age=0",
+        }
+        
+        paper = build(url, config=config)
+        
         for article in paper.articles:
             url_articulo = normalizar_url(article.url)
-
             if url_articulo in urls_pasadas:
-                print(f"Artículo ya procesado previamente, ignorado: {url_articulo}")
                 continue
 
             try:
-                article.download()
-                article.parse()
+                response = requests.get(url_articulo, headers=config.headers, timeout=10, allow_redirects=True)
+                if response.status_code == 200:
+                    article.download()
+                    article.parse()
+                    titulo = article.title
+                    fecha_publicacion = article.publish_date or 'Desconocido'
 
-                titulo = article.title
-                fecha_publicacion = article.publish_date
-
-                if not fecha_publicacion:
-                    fecha_publicacion = 'Desconocido'
+                    if fecha_publicacion != 'Desconocido':
+                        fecha_publicacion = fecha_publicacion.strftime('%Y-%m-%d')
+                        if fecha_mas_reciente and datetime.strptime(fecha_publicacion, '%Y-%m-%d') < fecha_mas_reciente:
+                            continue
+                    
+                    articulos.add((titulo, fecha_publicacion, url_articulo))
                 else:
-                    fecha_publicacion = fecha_publicacion.strftime('%Y-%m-%d')
-
-                    if fecha_mas_reciente and datetime.strptime(fecha_publicacion, '%Y-%m-%d') < fecha_mas_reciente:
-                        print("Artículo ignorado porque es más antiguo que la fecha más reciente registrada.")
-                        continue
-
-                articulos.append((titulo, fecha_publicacion, url_articulo))
-
+                    errores.append(f"Error {response.status_code} al descargar: {url_articulo}")
             except Exception as e:
-                print(f"Error procesando el artículo: {e}")
-
+                errores.append(f"Error procesando artículo {url_articulo}: {e}")
+            
     except Exception as e:
-        print(f"Error procesando el sitio: {e}")
+        errores.append(f"Error procesando sitio {url}: {e}")
 
+    if errores:
+        print("\nErrores encontrados:")
+        for error in errores:
+            print(error)
+
+    return articulos
+
+def procesar_varios_medios(urls):
+    start_time = time.time()
+    papers = [build(url, config=Config()) for url in urls]
+    news_pool.set(papers, threads_per_source=16)
+    news_pool.join()
+    print(f"--- {(time.time() - start_time):.2f}s seconds ---")
+    
+    articulos = set()
+    for paper in papers:
+        articulos.update(paper.article_urls())
+    
     return articulos
